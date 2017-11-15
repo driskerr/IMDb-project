68fc3e1347dbe0ab2017e56f4e6552d659680cc3
driskerr <kerrydriscoll06033@gmail.com>
2017-11-15 17:17:52 -0600

    Add files via upload

--- project.py
+++ project.py
@@ -11,20 +11,41 @@ import numpy as np
 import re
 import random
 import imdb
+from imdb import IMDb
+
 
 start_time = time.time()
 
-df = pd.DataFrame(columns=['ID', 'Title', 'Year', 'MPAA', 'Rating', 'Votes'])
+i =  IMDb(accessSystem='http')
 
-i =  imdb.IMDb(accessSystem='http')
-#"""
+demo_list=[]
+demo_list_sub=[]
+demos=i.get_movie('0780504','vote details').get('demographics')
+for d in demos:
+    demo_list.append(d)
+demo_list.remove('imdb staff')
+demo_list.remove('top 1000 voters')
+demo_list.remove('imdb users')
+demo_list = sorted(demo_list)
+myorder = [9, 4, 3, 0, 1, 2, 15, 14, 13, 10, 11, 12, 8, 5, 6, 7]
+demo_list = [demo_list[i] for i in myorder]
+for d in demo_list:
+    demo_list_sub.append(str(d)+'_rating')
+    demo_list_sub.append(str(d)+'_votes')
+
+
+df=pd.DataFrame()
+df_initial = pd.DataFrame(columns=['ID', 'Title', 'Year', 'MPAA', 'Rating', 'Votes'])
+
+i =  IMDb(accessSystem='http')
+"""
 movies = []
 for _ in range(100):
     randID = str(random.randint(0, 780505)).zfill(7)
     movies.append(randID)
-#"""
+"""
 #movies = ['0780504']
-#movies = ['0053494','0053494','4925292', '0780504', '0377092', '0268126', '0128853', '0050212', '0105435', '5013056', '0074119', '0064253', '0061811', '0112697', '0405094', '1255953', '0046268', '0029593']
+movies = ['0053494','0053494','4925292', '0780504', '0377092', '0268126', '0128853', '0050212', '0105435', '5013056', '0074119', '0064253', '0061811', '0112697', '0405094', '1255953', '0046268', '0029593']
 
 for m in movies:
     movie = i.get_movie(m)
@@ -54,14 +75,32 @@ for m in movies:
             mpaa=re.search('USA:(.+?)(:|,|\')',str(movie.get('certification'))).group(1)
     
         rating=movie.get('rating')
+        
+        demographics=i.get_movie(m,'vote details').get('demographics')
+        demo_dict={}
+        for d in demo_list:
+            if d in demographics:
+                demo_dict[str(d)+'_rating'] = demographics[d]['rating']
+                demo_dict[str(d)+'_votes']= demographics[d]['votes']
+            elif d not in demographics:
+                demo_dict[str(d)+'_rating'] = None
+                demo_dict[str(d)+'_votes']= 0
+        
+        demo_df=pd.DataFrame.from_dict(demo_dict, orient='index')
+        demo_df=demo_df.T
+        demo_df=demo_df[demo_list_sub]        
     
-        df = df.append(pd.DataFrame({'ID': [m], 'Title': [str(movie)], 'Year': [year], 'MPAA': [mpaa], 'Rating':[rating], 'Votes':[votes]}))
-
+        df_initial = pd.DataFrame({'ID': [m], 'Title': [str(movie)], 'Year': [year], 'MPAA': [mpaa], 'Rating':[rating], 'Votes':[votes]})
+        df_initial = df_initial[['ID', 'Title', 'Year', 'MPAA','Rating', 'Votes']]        
+        df_both = pd.concat([df_initial, demo_df], axis=1)
+        
+        df = df.append(df_both)
+    
 df = df.reset_index()    
 df['Year']=df['Year'].astype(int)
 df['Votes']=df['Votes'].astype(int)
-df = df[['ID', 'Title', 'Year', 'MPAA','Rating', 'Votes']]
 df = df.sort_values(by=['Rating'], ascending=False)
+df.reset_index
 
 run_time=time.time() - start_time
 print("--- {} seconds ---".format(run_time))
@@ -69,24 +108,8 @@ print("--- {} seconds per movie ---".format(run_time/len(movies)))
     
 #df = df.sort_values(by=['Year'])
 
+
 """
-i = imdb.IMDb(accessSystem='http')
-
-movie = i.get_movie('0780504')
-i.update(movie, 'vote details')
-print(movie.get('median'))
-print(movie.get('arithmetic mean'))
-print(movie.get('number of votes'))
-print(movie.get('demographics'))
-
-from imdb import IMDb
-ia = IMDb()
-m = ia.get_movie('0780504', 'vote details')
-print('median', m.get('median'))
-print('arithmetic mean', m.get('arithmetic mean'))
-print('number of votes', m.get('number of votes'))
-print('demographics', m.get('demographics'))
-
 from imdb import IMDb
 ia = IMDb()
 m = ia.get_movie('0780504', 'business')
